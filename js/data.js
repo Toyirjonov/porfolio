99aef39ea4a3d1ea33ebfae99d70dadbbf9d94c5
Toyirjonov <shoxruxmirzo12345@mail.ru>
2025-04-05 15:13:30 +0500

    complited

---
+++ js/data.js
@@ -0,0 +1,55 @@
+export default [
+  {
+    title: "Stats-preview-card-component",
+    github: "https://github.com/Toyirjonov/Stats-preview-card",
+    vercel: "https://stats-preview-card-one-pi.vercel.app/",
+  },
+
+  {
+    title: "Blog-preview-card",
+    github: "https://github.com/Toyirjonov/Blog-preview-card",
+    vercel: "https://blog-preview-card-psi-steel.vercel.app/",
+  },
+
+  {
+    title: "3-Column Preview Card",
+    github: "https://github.com/Toyirjonov/three-column-preview-card",
+    vercel: "https://three-column-preview-card-ten.vercel.app/",
+  },
+
+  {
+    title: "Social Links",
+    github: "https://github.com/Toyirjonov/Social-Links",
+    vercel: "https://social-links-mu-six.vercel.app/",
+  },
+
+  {
+    title: "Cafe Street",
+    github: "https://github.com/Toyirjonov/Cafe-Street",
+    vercel: "https://cafe-street-rho.vercel.app/",
+  },
+
+  {
+    title: "Power",
+    github: "https://github.com/Toyirjonov/Sport-web-site-Power",
+    vercel: "https://sport-web-site-power.vercel.app/",
+  },
+
+  {
+    title: "Recipe Page",
+    github: "https://github.com/Toyirjonov/Recipe-page",
+    vercel: "https://recipe-page-two-gamma.vercel.app/",
+  },
+
+  {
+    title: "AnsorMed",
+    github: "https://github.com/Toyirjonov/AnsorMed",
+    vercel: "https://ansor-med-mocha.vercel.app/",
+  },
+
+  {
+    title: "AkademNashr",
+    github: "https://github.com/Toyirjonov/Akademnashr",
+    vercel: "https://akademnashr-sable.vercel.app/",
+  },
+];
