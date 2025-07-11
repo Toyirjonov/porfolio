73cae4dddb147d697eced28b8a481f9d90201243
Toyirjonov <shoxruxmirzo12345@mail.ru>
2025-07-11 19:56:22 +0500

    add Shop

--- js/data.js
+++ js/data.js
@@ -1,13 +1,13 @@
 export default [
   {
+    title: "Product Shop",
+    github: "https://github.com/Toyirjonov/Shop",
+    vercel: "https://shop-fawn-sigma.vercel.app/login",
+  },
+  {
     title: "Context Shop",
     github: "https://github.com/Toyirjonov/react-context-practice",
     vercel: "https://react-context-practice-phi.vercel.app/",
-  },
-  {
-    title: "Product Shop",
-    github: "https://github.com/Toyirjonov/Shop",
-    vercel: "https://shop-three-mauve.vercel.app/",
   },
   {
     title: "CUsers",
